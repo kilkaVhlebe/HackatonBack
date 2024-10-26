412e08e3790780e70981b122615b58183f3ca148
Kilka_v_HJIebe <artempoznakov990@gmail.com>
2024-10-26 03:19:03 +0300

    add login and reg logic

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import UserService from './../service/user.service.js'
 
 const userService = new UserService()
-export const  user  = new Hono()
+export const  user  = new Hono().basePath('/user')
 
 user.post('/register', (context) => userService.userRegistration(context))
 user.post('/login', (context) => userService.userLogin(context))
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,16 +1,50 @@
+import type { Context } from "hono";
+import { createUser, getUserByLogin } from "../database.js";
+import { compare, genSalt, hash } from "bcrypt-ts";
+
 
 
 
 export default class UserService {
 
-async userRegistration(context:  any) {
+async userRegistration(context:  Context) {
+try {
+    const  request = await context.req.json()
+    if(!request.fullName || !request.login || !request.password ) {
+        return context.body("Registration failed", 400)
+    }
+    await genSalt(10)
+        .then((salt) => hash(request.password, salt))
+        .then((hash) => {
+            request.password = hash
+        });
+    await createUser(request)
+    return context.body("User registered", 200)
+} catch(error) {
+    console.error(error);
+    return context.body("Internal server error", 500)
+    
+}
 
-return context.text('Register user!')
+
 
 }
 
-async userLogin(context: any) {
-
+async userLogin(context: Context) {
+    try {    
+        const request = await context.req.json()
+        if(!request.login || !request.password) {
+            return context.body("Login failed", 400)
+        }
+        const user = await getUserByLogin(request.login)
+        if(!user) return context.body("Wrong login", 401)
+        return await compare(request.password, user.password).then((result) => 
+            result === true ? context.body("Login succesfull", 200) : context.body("Wrong password", 401)
+        )
+    } catch(error) {
+        console.error(error);
+        return context.body("Internal server error", 500)
+    }
 }
 
 }
