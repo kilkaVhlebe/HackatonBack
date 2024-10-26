5686bd605236a10187d6d0991a0564ad8eda0375
Kikatsuru <kikatsuru@gmail.com>
2024-10-26 10:19:24 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,22 @@ import { Hono } from 'hono'
 import { user}  from './router/user.router.js'
 import "dotenv/config"
 import { authMiddleware } from './middleware/auth.middleware.js'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
 app.use(authMiddleware)
+
+app.use(
+  cors({
+    origin: (origin, c) => origin,
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type' ],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
