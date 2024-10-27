b64adfa62f304c630d1aa35002a07be68a1e46e3
Kilka_v_HJIebe <artempoznakov990@gmail.com>
2024-10-27 13:24:12 +0300

    fix

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -2,7 +2,7 @@ import { cors } from 'hono/cors'
 
 export const corsMiddleware = cors({
     origin: (origin, c) => origin,
-    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type' ],
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type', 'Authorization' ],
     allowMethods: ['POST', 'GET', 'OPTIONS'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
     maxAge: 600,
